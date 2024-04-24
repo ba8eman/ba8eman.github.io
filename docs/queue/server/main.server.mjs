73049645a0d51fa53eaa0f39069a8e36ffeb4af1
Johnson Baby <pranav.chiddarwar@germinait.com>
2024-04-24 18:01:02 +0530

    deleted everything

--- docs/queue/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-K5O5UWU7.mjs";import"./chunk-5MFFCURX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-G4AL4ZQM.mjs";import"./chunk-OEH5W3D3.mjs";import"./chunk-HUNN5WPU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
